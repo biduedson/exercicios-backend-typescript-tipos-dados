6b98499b06904fd553188c2f82e941a4c9992e47
biduedson <112134265+biduedson@users.noreply.github.com>
2023-08-22 22:00:30 -0300

    feito exercicio 2

---
+++ 02/scripts.ts
@@ -0,0 +1,45 @@
+const usuarios2: { nome: string; idade: number; status: boolean }[] = [
+  {
+    nome: "Guido",
+    idade: 32,
+    status: true,
+  },
+  {
+    nome: "Dani",
+    idade: 30,
+    status: true,
+  },
+  {
+    nome: "João",
+    idade: 40,
+    status: false,
+  },
+  {
+    nome: "Guilherme",
+    idade: 29,
+    status: true,
+  },
+  {
+    nome: "Ana",
+    idade: 18,
+    status: false,
+  },
+  {
+    nome: "José",
+    idade: 28,
+    status: false,
+  },
+];
+
+const filtrarUsuarios = (
+  listaUsuarios: { nome: string; idade: number; status: boolean }[],
+  nome: string
+): { nome: string; idade: number; status: boolean }[] => {
+  let usuariosFiltros: { nome: string; idade: number; status: boolean }[] =
+    listaUsuarios.filter((usuario) => {
+      return usuario.nome.toLowerCase().includes(nome.toLowerCase());
+    });
+  return usuariosFiltros;
+};
+
+console.log(filtrarUsuarios(usuarios2, "w"));
