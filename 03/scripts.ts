256b246ed854c30e2082c7b254f03238b0989015
biduedson <112134265+biduedson@users.noreply.github.com>
2023-08-22 23:00:17 -0300

    modificando exercicio 3

--- 03/scripts.ts
+++ 03/scripts.ts
@@ -2,7 +2,7 @@ let arrayDeNumeros: number[] = [1, 5, 2];
 
 const tabuada = (arrayNumber: number[]): void => {
   for (let numero of arrayNumber) {
-    for (let i = 0; ij < 11; i++) {
+    for (let i = 0; i < 11; i++) {
       console.log(`${numero} x ${i} = ${numero * i}`);
     }
     console.log("----------------");
